9ceb7aa908943c9a86028e35b51138ca184f7148
hengyunabc <hengyunabc@gmail.com>
2018-12-25 01:07:47 +0800

    init

---
+++ src/test/java/com/alibaba/arthas/ArthasWebApplicationTests.java
@@ -0,0 +1,17 @@
+package com.alibaba.arthas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArthasWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
